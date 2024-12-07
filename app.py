7136e47b2a6b352a59c596ee6979c582ec29ef52
Fahmi <fahmiardiansyah959@gmail.com>
2024-12-08 00:00:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,15 @@ def get_instagram_data(username, password):
     L = instaloader.Instaloader()
 
     try:
-        # Login ke Instagram
         L.login(username, password)
     except Exception as e:
         raise Exception(f"Failed to login to Instagram: {e}")
 
-    # Ambil profil pengguna
     profile = instaloader.Profile.from_username(L.context, username)
 
-    # Ambil followers dan following
     followers = set(profile.get_followers())
     following = set(profile.get_followees())
 
-    # Kembalikan followers dan following
     return {
         'followers': [f.username for f in followers],
         'following': [f.username for f in following]
@@ -38,10 +34,7 @@ def get_data():
 
     try:
         data = get_instagram_data(username, password)
-        # Hitung unfollowers (yang mengikuti tetapi tidak diikuti)
         unfollowers = set(data['followers']) - set(data['following'])
         return jsonify({'followers': data['followers'], 'following': data['following'], 'unfollowers': list(unfollowers)})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-# Tidak perlu menjalankan app.run(debug=True) di Vercel, karena Gunicorn yang menjalankan aplikasi
